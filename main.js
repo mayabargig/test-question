61296b37d1f4a889a08b667b4293eef66351dbd6
mayabargig <mayabargig@gmail.com>
2023-09-07 20:01:45 +0300

    first

---
+++ main.js
@@ -0,0 +1,31 @@
+var counterWidth= 50;
+var counterHeight= 50;
+var interval;
+
+
+function getValuesFromUser (){
+    var divWidth = myWidth.value;
+    var divHeight = myHeight.value;
+
+    interval = setInterval(function(){
+        if (counterWidth < divWidth){
+            counterWidth++;
+            myDiv.style.width= counterWidth + "px";
+        }
+        if(counterHeight < divHeight){
+            counterHeight++;
+            myDiv.style.height = counterHeight + "px";
+        }
+    
+        if(counterWidth === divWidth && counterHeight === divHeight){
+            alert("the result!");
+            clearInterval(interval);
+        }
+        }, 100);
+
+        return false;
+    }
+
+function stopGrowing() {
+    clearInterval(interval);
+}
